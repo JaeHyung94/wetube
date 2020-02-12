84f9951da49091ebe48a9dc64aed7c9d7c475cef
JaeHyung_iMac <jhpark.main@gmail.com>
2020-02-12 19:24:36 +0900

    install middlewares

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hi");
+import express from "express";
+import logger from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from lab");
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(logger("dev"));
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
