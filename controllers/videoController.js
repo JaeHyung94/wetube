5c749213cdb9a200fe4749f2f7792c84df275e0a
JaeHyung_iMac <jhpark.main@gmail.com>
2020-02-14 11:11:51 +0900

    MVC pattern -making View files #1

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,8 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("Search");
 
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("video detail");
-export const editVideo = (req, res) => res.send("video edit");
-export const deleteVideo = (req, res) => res.send("delete video");
+export const videos = (req, res) => res.render("videos");
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("video detail");
+export const editVideo = (req, res) => res.render("video edit");
+export const deleteVideo = (req, res) => res.render("delete video");
