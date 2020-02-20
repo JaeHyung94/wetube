ffb1c84c4a73ace1f90ab09c37818937e55e08a1
JaeHyung_iMac <jhpark.main@gmail.com>
2020-02-20 22:01:52 +0900

    MVC Pattern: Model with MongoDB and Mongoose #1 - Video Model

--- db.js
+++ db.js
@@ -1,54 +1,18 @@
-export const videos = [
-  {
-    id: 32549,
-    title: `Video Awesome`,
-    description: "This is something i love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 1357924,
-      name: "Jay",
-      email: "Jay@Park.com"
-    }
-  },
-  {
-    id: 2788804,
-    title: `Video Fantastic`,
-    description: "This is something i like",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 3909174,
-      name: "Jay",
-      email: "Jay@Park.com"
-    }
-  },
-  {
-    id: 1844427,
-    title: `Video Good`,
-    description: "This is something i want to eat",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 2323093,
-      name: "Jay",
-      email: "Jay@Park.com"
-    }
-  },
-  {
-    id: 9008723,
-    title: `Video Zec`,
-    description: "This is something i want to buy",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 5556778,
-      name: "Jay",
-      email: "Jay@Park.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB!");
+const handleError = error => console.log(`Error on DB Connection!:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
