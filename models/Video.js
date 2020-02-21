2bc14a586f78c119fbaa1558a0207a476d2fe1e3
JaeHyung_iMac <jhpark.main@gmail.com>
2020-02-21 16:48:52 +0900

    MVC Pattern: Model w/ MongoDB and Mongoose #2 - Comment Model

--- models/Video.js
+++ models/Video.js
@@ -17,7 +17,13 @@ const VideoSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "comments"
+    }
+  ]
 });
 
 const model = mongoose.model("Video", VideoSchema);
